3817fbb8ecee62717a35c23b35bd1dc06a318b18
MacLovin <monteiro9961@gmail.com>
2021-06-23 16:21:13 -0300

    Method reference static

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -28,6 +28,12 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+
+
 
 	@Override
 	public String toString() {
